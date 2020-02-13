93eddfeb137ddac48d6e700a288edfd80f5c416f
YuriySuro <suro1989@ukr.net>
2020-02-13 12:35:06 +0200

    react createContext

--- src/components/app.js
+++ src/components/app.js
@@ -15,6 +15,9 @@ import {
   StarshipDetails
 } from './sw-components/details'; 
 import SwapiService from './services/index';
+import DummySwapiService from './services/dummy-swapi-service';
+import { SwapiServiceProvider } from './swapi-service-context/swapi-service-context';
+
 import '../styles/index.css';
 import '../styles/bootstrap.min.css';
 
@@ -23,6 +26,7 @@ import '../styles/bootstrap.min.css';
 export default class App extends PureComponent {
 
   swapiService = new SwapiService();
+  dummySwapiService = new DummySwapiService();
 
   state = {
     showRandomFilm: true,
@@ -52,21 +56,24 @@ export default class App extends PureComponent {
 
     return (
       <ErrorBoundary>
-        <div className="swapi-app">
-            <Header />
-            <PersonDetails itemId={4} />
-            <PlanetDetails itemId={5} />
-            <StarshipDetails itemId={5} />
-            <hr style={{background:"#444"}} />
-            <PersonList />
-            <hr style={{background:"#444"}} />
-            <PlanetList />
-            <hr style={{background:"#444"}} />
-            <StarshipList />
+        <SwapiServiceProvider value={this.dummySwapiService}>
+          <div className="swapi-app">
+              <Header />
+              <PersonDetails itemId={4} />
+              <PlanetDetails itemId={5} />
+              <StarshipDetails itemId={9} />
+              <hr style={{background:"#444"}} />
+              <PersonList />
+              <hr style={{background:"#444"}} />
+              <PlanetList />
+              <hr style={{background:"#444"}} />
+              <StarshipList />
 
-            <Footer />
-        </div>
+              <Footer />
+          </div>
+        </SwapiServiceProvider>
       </ErrorBoundary>
+
     );
   };
 };
